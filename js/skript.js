c93e2f92024522e3b18b5063a9b3bf98d98eda16
Yegor <nebris85@gmail.com>
2022-11-23 00:22:26 +0200

    practice1

--- js/skript.js
+++ js/skript.js
@@ -1,1 +1,17 @@
-"use strict";
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+const a = prompt("Один из просмотреных фильмов?", ""),
+  b = prompt("На сколько оцените его?", ""),
+  c = prompt("Один из просмотреных фильмов?", ""),
+  d = prompt("На сколько оцените его?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
