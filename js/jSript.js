39838bbf77e474061a2a1e9a38e67b8d0c102c7d
Yegor <nebris85@gmail.com>
2023-02-07 15:19:54 +0200

    ver 0.1

--- js/jSript.js
+++ js/jSript.js
@@ -18,7 +18,7 @@ for (let i = 0; i < 2; i++) {
   const a = prompt("Один из просмотреных фильмов?", ""),
         b = prompt("На сколько оцените его?", "");
         personalMovieDB.movies[a] = b;
-};
+}
 
 console.log(personalMovieDB); 
 
