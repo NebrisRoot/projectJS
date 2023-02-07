818f25dcd5b6beef02d3e5a9b23916382d1f3b46
Yegor <nebris85@gmail.com>
2023-02-07 15:15:01 +0200

    Last var

--- js/jSript.js
+++ js/jSript.js
@@ -6,13 +6,19 @@ const personalMovieDB = {
   genres: [],
   privat: false,
 };
-const a = prompt("Один из просмотреных фильмов?", ""),
-  b = prompt("На сколько оцените его?", ""),
-  c = prompt("Один из просмотреных фильмов?", ""),
-  d = prompt("На сколько оцените его?", "");
+// const a = prompt("Один из просмотреных фильмов?", ""),
+//   b = prompt("На сколько оцените его?", ""),
+//   c = prompt("Один из просмотреных фильмов?", ""),
+//   d = prompt("На сколько оцените его?", "");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+
+for (let i = 0; i < 2; i++) {
+  const a = prompt("Один из просмотреных фильмов?", ""),
+        b = prompt("На сколько оцените его?", "");
+        personalMovieDB.movies[a] = b;
+};
 
 console.log(personalMovieDB); 
 
